0d408038e13002af4fe35f05487ad764079c5aba
kathiravan-018 <kathir121006@gmail.com>
2026-08-29 21:10:39 +0530

    Fix frontend build

--- ai_frontend/src/features/QuizGenerator.jsx
+++ ai_frontend/src/features/QuizGenerator.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useQuiz from "../hooks/useQuiz";
-import { useNavigate } from "react-router-dom";
+
 
 export default function QuizGenerator() {
 
